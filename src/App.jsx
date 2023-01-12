2da252b128a12b58fb1283ca018dfc83c132ce53
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2023-01-12 00:31:59 -0300

    Esqueleto de proyecto (html), se agregan carpetas 'api' y 'components' para gestion de organizacion

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,39 @@ const App = () => {
   
 
   return (
-    "App"
+    <>
+      <form action="">
+        <label htmlFor="">Búsqueda:</label>
+        <input type="text" />
+      </form>
+      <hr />
+      <form>
+        <label htmlFor="">Nombre del Colaborador: </label>
+        <input type="text" name="" id="" placeholder="nombre"/>
+        <br />
+        <label htmlFor="">Correo colaborador: </label>
+        <input type="text" name="" id="" placeholder="correo"/>
+        <br />
+        <button type="submit">Agregar colaborador</button>
+      </form>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Nombre</td>
+            <td>Correo</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>0</td>
+            <td>Colaborador 0</td>
+            <td>colaborador0@colaborador0.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
